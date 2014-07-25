a0e7df465cef506c6fda717aebb62bed23642102
quelledanielle <quelledanielle@gmail.com>
2014-07-25 16:45:48 -0400

    Copy directory contents instead of directory

--- myvcs.js
+++ myvcs.js
@@ -9,41 +9,42 @@ if (!fs.existsSync(VCS)) fs.mkdirSync(VCS);
 
 function backup() {
   fs.mkdir(nextBackup());
-  cpdirSync(CWD, path.join(latestBackup(), path.basename(CWD)));
+  fs.mkdir(path.join(nextBackup(), path.basename(CWD)));
+  cpContents(CWD, path.join(latestBackup(), path.basename(CWD)));
 }
 
 function checkout(n) {
   if (!n) throw new Error('Checkout number not provided');
   cleardir(CWD);
-  cpdirSync(path.join(CWD, VCS, n.toString()), path.join(CWD, '..'));
+  cpContents(path.join(CWD, VCS, n.toString(), path.basename(CWD)), path.join(CWD));
 }
 
 /**
  * Copying files and directories
  */
-function cpFileSync(fIn, fOut) {
+function cpFile(fIn, fOut) {
   var data = fs.readFileSync(fIn, { encoding: 'utf8' });
   fs.writeFileSync(fOut, data);
 }
 
-function cpdirSync(dIn, dOut) {
-  var stats = fs.lstatSync(dIn);
-  if (stats.isDirectory()) {
-    console.log('Copying directory:', dIn);
+function cpContents(dIn, dOut) {
+  console.log('Copying contents:', dIn);
 
-    // Create backup directory
-    if (!fs.existsSync(dOut)) fs.mkdirSync(dOut);
+  var ls = fs.readdirSync(dIn);
+  ls.forEach(function(file) {
+    if (file === VCS) return;
+    var fIn = path.join(dIn, file);
+    var fOut = path.join(dOut, file);
+    var stats = fs.lstatSync(fIn);
 
-    // Recursively copy files in current directory
-    var ls = fs.readdirSync(dIn);
-    ls.forEach(function(file) {
-      if (file === VCS) return;
-      cpdirSync(path.join(dIn, file), path.join(dOut, file));
-    });
-  } else if (stats.isFile()) {
-    console.log('Copying file:', dIn);
-    cpFileSync(dIn, dOut);
-  }
+    if (stats.isDirectory()) {
+      fs.mkdirSync(fOut);
+      cpContents(fIn, fOut);
+    } else if (stats.isFile()) {
+      console.log('Copying file:', fIn);
+      cpFile(fIn, fOut);
+    }
+  });
 }
 
 /**
@@ -51,21 +52,22 @@ function cpdirSync(dIn, dOut) {
  */
 function cleardir(dir) {
   var ls = fs.readdirSync(dir);
-  
-  // Remove empty directory
-  if (ls.length === 0) return fs.rmdirSync(dir);
 
   ls.forEach(function(file) {
-    if (!file || file === VCS) return;
+    if (file === VCS) return;
     var filepath = path.join(dir, file);
     var stats = fs.lstatSync(filepath);
 
-    // Recursively clear directory
-    if (stats.isDirectory()) cleardir(filepath);
-
-    // Remove file
-    if (stats.isFile()) fs.unlinkSync(filepath);
+    if (stats.isDirectory()) {
+      cleardir(filepath);
+    } else if (stats.isFile()) {
+      fs.unlinkSync(filepath);
+    }
   });
+
+  if (dir !== CWD) {
+    fs.rmdirSync(dir);
+  }
 }
 
 /**
@@ -101,6 +103,10 @@ switch (args[0]) {
   case 'latest':
     checkout(latestIndex());
     break;
+  case 'clear':
+    cleardir(CWD);
+    break;
   default:
     throw new Error('Invalid command');
 }
+
